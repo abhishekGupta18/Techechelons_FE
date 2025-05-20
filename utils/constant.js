9c21e94403ce91ce1ea2af46a7c41a5168d152c6
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-05-20 11:31:56 +0530

    updated base url

--- utils/constant.js
+++ utils/constant.js
@@ -1,4 +1,4 @@
 export const baseURL =
   location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://connectdev-be.onrender.com";
+    : "https://techechelons-be.onrender.com/";
